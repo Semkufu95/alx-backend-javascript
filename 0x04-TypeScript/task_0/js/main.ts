45f69b8b3148e235b9ea69b27750a46185cb433d
semkufu95 <semkufu95@gmail.com>
2024-10-09 18:54:57 +0300

    Task 0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,3 +1,4 @@
+// An interface named Student that accepts firstname, lastname, age and location
 
 interface Student {
     firstName: string;
@@ -5,3 +6,35 @@ interface Student {
     age: number;
     location: string;
 };
+
+//create two students
+
+const firstStudent: Student {
+    firstName: 'Roby',
+    lastName: 'Ngas',
+    age: 25,
+    location: 'Mwanza',
+};
+
+const secondStudent: Student {
+    firstName: 'Rauhiyyah',
+    secondName: 'Habibu',
+    age: 20,
+    location: 'Pemba',
+
+// create an array named studentsList
+
+studentsList = [firstStudent, secondStudent]
+
+// REnder with Vanilla JavaScript
+ const table = document.getElementById('tableStudents') as HTMLTableElement:
+
+// create a new row
+
+studentsList.forEach((student) = {
+   const row = table.insertRow();
+   const firstNameCell = row.insertCell(0);
+   const locationCell = row.insertCell(1);
+   firstNameCell.innerHTML = student.firstName;
+   locationCel.innerHTML = student.location;
+   })
