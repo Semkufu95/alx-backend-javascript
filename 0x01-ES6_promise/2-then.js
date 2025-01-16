e2417bfe655d6c98305d1f73b5f58d3bdd98a72e
semkufu95 <semkufu95@gmail.com>
2025-01-16 16:12:58 +0300

    Refactored code for task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else {
-      reject(new Error());
-    }
-  })
-    .then(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
